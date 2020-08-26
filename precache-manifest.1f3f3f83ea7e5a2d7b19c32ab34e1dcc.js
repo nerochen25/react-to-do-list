1981978458000ac656e216189890e0f35abea663
Nero <nerochen25@berkeley.edu>
2020-08-26 02:56:17 -0700

    Updates

---
+++ precache-manifest.1f3f3f83ea7e5a2d7b19c32ab34e1dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692d4f6f56a4e19e7b0c7f67ae80a193",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "91d27f6853e502e90cc1",
+    "url": "/react-to-do-list/static/css/main.4ab6e4b4.chunk.css"
+  },
+  {
+    "revision": "9f2a4de149c341b878c5",
+    "url": "/react-to-do-list/static/js/2.4ccf0b92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.4ccf0b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d27f6853e502e90cc1",
+    "url": "/react-to-do-list/static/js/main.87c2ff5e.chunk.js"
+  },
+  {
+    "revision": "e191761989334030c98f",
+    "url": "/react-to-do-list/static/js/runtime-main.0e5a2281.js"
+  }
+]);
